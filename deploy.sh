3dfa2c3c390439093e5224b1c95b75d9e9e0c34a
mstssk <masato.sasaki+github@topgate.co.jp>
2017-09-13 20:44:33 +0900

    oauth2_access_token

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,5 @@ VERSION=`git rev-parse --abbrev-ref HEAD`
 gb gae appcfg update \
   --application=$PROJECT \
   --version=$VERSION \
+  --oauth2_access_token $(gcloud auth print-access-token 2> /dev/null) \
   src/
