316e124fe8b6969b76d50dad87b25763e33f2284
mstssk <mstssk+github@gmail.com>
2017-11-11 10:55:11 +0900

    setup

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ cd `dirname $0`
 go get -u github.com/constabulary/gb/...
 go get -u github.com/PalmStoneGames/gb-gae
 
+go get -u sourcegraph.com/sqs/goreturns
 go get -u github.com/golang/lint/golint
