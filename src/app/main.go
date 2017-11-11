9bc02e48a542f84f8fae0f02736199f0e53ab941
mstssk <mstssk+github@gmail.com>
2017-11-11 13:23:03 +0900

    health detail

--- src/app/main.go
+++ src/app/main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"net/http"
 	"runtime"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
 func init() {
-	http.HandleFunc("/api/health", handleAPIHealth)
+	http.HandleFunc("/api/admin/health", handleAPIHealth)
 	http.HandleFunc("/", handler)
 }
 
@@ -17,9 +20,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleAPIHealth(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	log.Debugf(c, "%#v", c)
+
 	w.Header().Set("Content-Type", "application/json")
 	res := map[string]string{
-		"runtime_version": runtime.Version(),
+		"version":                  runtime.Version(),
+		"app_id":                   appengine.AppID(c),
+		"module_name":              appengine.ModuleName(c),
+		"version_id":               appengine.VersionID(c),
+		"datacenter":               appengine.Datacenter(c),
+		"default_version_hostname": appengine.DefaultVersionHostname(c),
+		"instance_id":              appengine.InstanceID(),
+		"server_software":          appengine.ServerSoftware(),
 	}
 	json, _ := json.Marshal(res)
 	fmt.Fprint(w, string(json))
